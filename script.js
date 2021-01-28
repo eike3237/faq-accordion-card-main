5c861a29f553c7a325b58023536fe0f0f5e54985
eike3237 <eike3237@users.noreply.github.com>
2021-01-27 22:43:17 -0300

    Desktop Version Finished

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function showAnswer (id) {
+function showAnswer (id, faq_quest) {
+
     if (document.getElementById(id).style.display === 'block') {
         document.getElementById(id).style.display = 'none';
+        document.getElementById(faq_quest).style.color = 'hsl(237, 12%, 33%)'; /*Terminar de colocar Weight na cor pra dar destaque, alterando somente o primeiro elemento*/
+        document.getElementById(faq_quest).style.fontWeight = 400;
     } else {
         document.getElementById(id).style.display = 'block';
+        document.getElementById(faq_quest).style.color = 'hsl(238, 29%, 16%)';
+        document.getElementById(faq_quest).style.fontWeight = 700;        
     }
 }
